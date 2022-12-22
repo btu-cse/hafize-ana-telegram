686d84b9c8e9817e86e9a5e6e2ad0238f27ea3a1
Arda Aydın Kılınç <113470792+adraarda23@users.noreply.github.com>
2022-12-22 16:59:14 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
 import sqlite3
 from datetime import datetime
 
+
 class Models:
 
-    def __init__(self):
-        self.db =  sqlite3.connect('database.db',check_same_thread=False)
-        self.cursor = self.db.cursor()
-        self.create_table()
+  def __init__(self):
+    self.db = sqlite3.connect('database.db', check_same_thread=False)
+    self.cursor = self.db.cursor()
+    self.create_table()
 
-    def create_table(self):
-        """Creates table if not exists"""
-        self.cursor.execute('''
+  def create_table(self):
+    """Creates table if not exists"""
+    self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS subscriptions (
             chat_id TEXT PRIMARY KEY NOT NULL, 
             first_name TEXT DEFAULT "",
@@ -18,32 +19,56 @@ class Models:
             subsdate DATETIME DEFAULT CURRENT_TIMESTAMP
             )
         ''')
-        self.db.commit()
+    self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY,
+            chat_id TEXT NOT NULL, 
+            first_name TEXT DEFAULT "",
+            last_name TEXT DEFAULT "",
+            message TEXT DEFAULT "",
+            message_date DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+    self.db.commit()
 
-    def add_user(self, chat_id, first_name, last_name):
-        """Adds people to database"""
-        self.cursor.execute('''
+  def add_user(self, chat_id, first_name, last_name):
+    """Adds people to database"""
+    self.cursor.execute(
+      '''
         INSERT INTO subscriptions (chat_id, first_name, last_name) VALUES (?, ?, ?)
         ''', (chat_id, first_name, last_name))
-        self.db.commit()
+    self.db.commit()
 
-    def delete_person(self, chat_id):
-        """Deletes people from database"""
-        self.cursor.execute('''
+  def delete_person(self, chat_id):
+    """Deletes people from database"""
+    self.cursor.execute(
+      '''
         DELETE FROM subscriptions WHERE chat_id = ?
-        ''', (chat_id,))
-        self.db.commit()
-        
-    def check_person(self, chat_id):
-        """Checks if people exists"""
-        self.cursor.execute('''
-        SELECT * FROM subscriptions WHERE chat_id = ?
-        ''', (chat_id,))
-        return self.cursor.fetchone()
+        ''', (chat_id, ))
+    self.db.commit()
 
-    def check_all(self):
-        self.cursor.execute('''
+  def check_person(self, chat_id):
+    """Checks if people exists"""
+    self.cursor.execute(
+      '''
+        SELECT * FROM subscriptions WHERE chat_id = ?
+        ''', (chat_id, ))
+    return self.cursor.fetchone()
+
+  def check_all(self):
+    self.cursor.execute('''
         SELECT * FROM subscriptions 
         ''')
-        return self.cursor.fetchall()
-        
+    return self.cursor.fetchall()
+
+  def add_message(self, chat_id, first_name, last_name, message):
+    self.cursor.execute(
+      "INSERT INTO messages (chat_id, first_name, last_name,message) VALUES (?, ?, ?, ?)",
+      (chat_id, first_name, last_name, message))
+    self.db.commit()
+
+  def get_all_messages(self):
+    self.cursor.execute('''
+        SELECT * FROM messages
+        ''')
+    return self.cursor.fetchall()
